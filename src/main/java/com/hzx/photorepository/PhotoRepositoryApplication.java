41bba4f70ccf308997bf32ff23ac01725561544a
Hzx6 <2811409020@qq.com>
2026-05-01 15:00:19 +0800

    本地测试提交

---
+++ src/main/java/com/hzx/photorepository/PhotoRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.hzx.photorepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzx/photorepository/PhotoRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzx.photorepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
