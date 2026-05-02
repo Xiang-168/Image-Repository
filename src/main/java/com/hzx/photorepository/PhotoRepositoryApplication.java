0284c535c0f6490950d98610dd67a5a6c4f0d139
Hzx6 <2811409020@qq.com>
2026-05-02 14:11:44 +0800

    后端项目初始化

--- src/main/java/com/hzx/photorepository/PhotoRepositoryApplication.java
+++ src/main/java/com/hzx/photorepository/PhotoRepositoryApplication.java
@@ -1,9 +1,13 @@
 package com.hzx.photorepository;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@MapperScan("com.hzx.photorepository.mapper")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class PhotoRepositoryApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/hzx/photorepository/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.hzx.photorepository.controller;
+
+import com.hzx.photorepository.common.BaseResponse;
+import com.hzx.photorepository.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+
+}
