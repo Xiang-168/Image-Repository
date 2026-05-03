5dff2e2db59e968a1bd3c9bffa0ed46ed1c02a96
Hzx6 <2811409020@qq.com>
2026-05-03 21:17:34 +0800

    合并前后端代码：后端移入 photo-repository-banked 目录，前端放入 photo-repository-front 目录

---
+++ photo-repository-front/src/layout/BasicLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/Xiang-168/Image-Repository" target="_blank">github代码网址</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+<script setup lang="ts">
+import GlobalHeader from '@/components/GlobalHeader.vue'
+</script>
+<style scoped>
+#basicLayout .header {
+  padding-inline: 20px;
+  background: white;
+  color:unset;
+  margin-bottom: 16px;
+}
+
+#basicLayout .content {
+  padding: 40px;
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+}
+
+#basicLayout .footer {
+  background-color: #efefef;
+  padding: 18px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
